31f30c162b2178c7e4262de72a2e251235f910de
SergeySmirnov75 <122604382+SergeySmirnov75@users.noreply.github.com>
2023-03-15 16:16:47 +0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,88 @@
-package org.example;
-
 public class Main
 {
-    public static void main(String[] args)
-    {
-        //System.out.println("Hello world!");
+    public static void main(String[] args) {
+        ArrEmployee arrEmployee = new ArrEmployee();
 
-        /*
-        Расскажите про String в Java.
+        arrEmployee.addEmployer("Смирнов Сергей Николаевич", 1, 95000);
+        arrEmployee.addEmployer("Киселёв Иван Киприянович", 1, 87000);
+        arrEmployee.addEmployer("Ушаков Павел Владимирович", 1, 89000);
+        arrEmployee.addEmployer("Максимов Александр Юрьевич", 2, 89000);
+        arrEmployee.addEmployer("Калинин Виталий Владимирович", 3, 110000);
+        arrEmployee.addEmployer("Заика Андрей Олегович", 3, 67000);
+        arrEmployee.addEmployer("Завалин Александр Сергеевич", 4, 250000);
+        arrEmployee.addEmployer("Патерюхин Артём Алексеевич", 2, 90000);
 
-        В чем разница между == и equals()?
 
-        Какая функция у оператора **return?**
+        arrEmployee.printFullName();
 
-        - Задача
+        System.out.println();
 
-          Необходимо реализовать следующий метод:
+        System.out.println("Общая трата на зарплаты " + arrEmployee.calculateSumSallary());
 
-          1. Получаем на входе массив чисел.
+        System.out.println();
 
-          2. Все четные числа увеличиваем на единицу.
+        System.out.println("Минимальная зарплата " + arrEmployee.minSallary());
 
-           3. Возвращаем кусок списка с 3-го по 7-й элемент.
-          */
+        System.out.println();
 
-        int [] mass = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int i;
-        for (i = 0; i < mass.length; i++)
-            if (mass[i] % 2 == 0)
-                mass[i] = mass[i]++;
-        for (i = 2; i < 7; i++)
-            System.out.print(mass[i] + " ");
+        System.out.println("Максимальная зарплата " + arrEmployee.maxSallary());
+
+        System.out.println();
+
+        System.out.println("Средняя зарплата " + arrEmployee.calculateAvgSallary());
+
+        System.out.println();
+
+        arrEmployee.printAllEmployer();
+
+        System.out.println();
+
+        arrEmployee.changeSallaryPerson(0, 190000);
+
+        System.out.println();
+
+        arrEmployee.changeDepartamentPerson(0, 3);
+
+        System.out.println();
+
+        arrEmployee.changeSallaryIndex(10);
+
+        System.out.println();
+
+        arrEmployee.printAllEmployer();
+
+        System.out.println();
+
+        arrEmployee.minSallaryDepartament(4);
+
+        System.out.println();
+
+        arrEmployee.maxSallaryDepartament(4);
+
+        System.out.println();
+
+        arrEmployee.sumSallaryDepartament(4);
+
+        System.out.println();
+
+        arrEmployee.avgSallaryDepartament(4);
+
+        System.out.println();
+
+        arrEmployee.changeSallaryIndexDepartament(4, 15);
+
+        System.out.println();
+
+        arrEmployee.printInfoDepartament(4);
+
+        System.out.println();
+
+        arrEmployee.printMinSallaryOfNum(100000);
+
+        System.out.println();
+
+        arrEmployee.printMaxSallaryOfNum(100000);
+
 
     }
 }
